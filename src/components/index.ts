e48ffd618acca5c02140d60bb2b249a47ba9b030
jonatas-melo-silva <jonatas891024@gmail.com>
2022-02-24 23:22:07 -0300

    :tada: Desafio Finalizado

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import { Header } from './Header'
+import { SideBar } from './SideBar'
+import { Content } from './Content'
+import { Icon } from './Icon'
+import { MovieCard } from './MovieCard'
+import { Button } from './Button'
+
+export {
+  Header,
+  SideBar,
+  Content,
+  Icon,
+  MovieCard,
+  Button
+}
